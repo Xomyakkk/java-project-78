a4286a6fe553dde2161559b9bc48364ea1c0abf1
Alex <megaxomyak@gmail.com>
2026-05-10 19:04:19 +0300

    add version dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
