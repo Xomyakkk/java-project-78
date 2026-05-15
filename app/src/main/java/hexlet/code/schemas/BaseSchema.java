3f94bbb15abdaa58da4a4934c8cf3f6d92e32fda
Alex <megaxomyak@gmail.com>
2026-05-15 08:42:47 +0300

    Add Javadoc for BaseSchema

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
+/**
+ * Базовый класс для схем валидаторов.
+ *
+ * @param <T> тип значения проверяется схемой.
+ */
 public abstract class BaseSchema<T> {
     protected Map<String, Predicate<T>> checks = new HashMap<>();
     protected boolean required = false;
@@ -12,6 +17,16 @@ public abstract class BaseSchema<T> {
         checks.put(name, validate);
     }
 
+    /**
+     * Отмечает значение, соответствующее текущей схеме.
+     *
+     * <p>Подклассы могут переопределять этот метод только для того,
+     *      чтобы сузить тип возвращаемого значения для гибкого API.
+     * Подобные переопределения должны вызывать {@code super.required()}
+     * и сохранить то же самое поведение.</p>
+     *
+     * @return текущий экземпляр схемы
+     */
     public BaseSchema<T> required() {
         required = true;
         return this;
